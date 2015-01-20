c1bf38315e8c3ef4674debb62015545c617f0e88
Steve Witman <stevewitman@gmail.com>
2015-01-20 13:03:19 -0700

    added code to manipulate the audio src

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ $(document).ready(function() {
     $(this).removeClass('fa-play').addClass('fa-stop');
     var song_id = $(this).data('id');
     var song_title = $(this).data('title');
-    $('audio').each(function(){
-      this.pause();
-    });
+    document.getElementById(song_id).src = "";
+    var song_src = $(this).data('src');
+    document.getElementById(song_id).src = "tracks/" + song_src + ".mp3";
     document.getElementById(song_id).play();
     $("#heading").text("Now playing: " + song_title);
   });
@@ -24,7 +24,7 @@ $(document).ready(function() {
     $(this).removeClass('fa-stop').addClass('fa-play');
     var song_id = $(this).data('id');
     document.getElementById(song_id).pause();
-    document.getElementById(song_id).currentTime = 1;
+    document.getElementById(song_id).src = "";
     $("#heading").text("Select a song!");
   });
 
