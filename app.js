6a193c879fdc34ba77982c0bd5e0ef4d2d39f9ec
Steve Witman <stevewitman@gmail.com>
2015-01-20 15:40:11 -0700

    used trigger to play and pause

--- app.js
+++ app.js
@@ -1,31 +1,25 @@
 "use strict";
 
 $(document).ready(function() {
-
   $.getJSON('data.json', function(tracks) {
     var $tracksTemplate = $('#tracksTemplate').html();
     var newHTML = Mustache.to_html($tracksTemplate, tracks);
     $('.tracks').html(newHTML);
   });
-
   $('.container').on('click', '.fa-play', function() {
     $('.fa').removeClass('fa-stop').addClass('fa-play');
     $(this).removeClass('fa-play').addClass('fa-stop');
     var song_id = $(this).data('id');
     var song_title = $(this).data('title');
-    document.getElementById(song_id).src = "";
     var song_src = $(this).data('src');
-    document.getElementById(song_id).src = "tracks/" + song_src + ".mp3";
-    document.getElementById(song_id).play();
+    var song_src_path = "tracks/" + song_src + ".mp3";
+    $("#player").attr("src",song_src_path);
+    $('#player').trigger("play");
     $("#heading").text("Now playing: " + song_title);
   });
-
   $('.container').on('click', '.fa-stop', function() {
     $(this).removeClass('fa-stop').addClass('fa-play');
-    var song_id = $(this).data('id');
-    document.getElementById(song_id).pause();
-    document.getElementById(song_id).src = "";
+    $("#player").trigger("pause");
     $("#heading").text("Select a song!");
   });
-
 });
